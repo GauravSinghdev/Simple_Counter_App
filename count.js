4fd5797cb0365a2256fb4463c5004b4daaedd38d
GauravSinghdev <gauravkalakoti19@gmail.com>
2024-03-17 17:49:03 +0530

    updated with greeting msg

--- count.js
+++ count.js
@@ -18,3 +18,28 @@ function reset()
     clearInterval(timer);
     document.getElementById("init").innerHTML = 0;
 }
+
+
+function greeting()
+{
+    const date = new Date();
+    const hour = date.getHours();
+
+    if(hour>=5 && hour<12)
+    {
+        greeting = "Good Morning there";
+    }
+    else if(hour>12 && hour>18)
+    {
+        greeting = "Goode Afternoon there";
+    }
+    else{
+        greeting = "Good Evening there";
+    }
+
+    return greeting;
+}
+
+const greet = document.getElementById("greeting-msg");
+
+greet.innerHTML = greeting();
