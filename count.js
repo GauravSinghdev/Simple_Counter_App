b4dbd396066fe3b0617f33a4b09108797e26652d
GauravSinghdev <gauravkalakoti19@gmail.com>
2024-03-17 21:00:40 +0530

    updated time format

--- count.js
+++ count.js
@@ -43,3 +43,26 @@ function greeting()
 const greet = document.getElementById("greeting-msg");
 
 greet.innerHTML = greeting();
+
+
+function getdate()
+{
+    let date = new Date();
+    let date1 = date.getHours();
+    var dt;
+    if(date1>0 && date1<12)
+    {
+        dt  = "AM";
+    }
+    else dt = "PM";
+
+    let hour = date1 - 12;
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    const d = document.getElementById("showdate");
+
+    d.innerHTML =  `${hour}:${min}:${sec} ${dt}`;
+}
+
+
+setInterval(getdate,10);
